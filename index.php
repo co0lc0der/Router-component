c16ef8cf9568f95fb3cf68054c2c95e49292960d
co0lc0der <c0der@ya.ru>
2021-04-13 22:46:27 +0500

    index.php edited

--- index.php
+++ index.php
@@ -2,6 +2,5 @@
 $config = require __DIR__ . '/Router/config.php';
 require __DIR__ . '/Router/Router.php';
 
-var_dump($_SERVER);var_dump($config);die;
 $router = new Router($config['routes'], $config['tplpath']);
 $router->run();
